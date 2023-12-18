c765ab85186040eed411944feb4d22b68908d40b
Nestana <kasymovanestana776@gmail.com>
2023-12-18 21:06:19 +0600

    'nesti'

--- src/Components/cart_view_all.js
+++ src/Components/cart_view_all.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './cart_view_all.scss'
 
+
+
 const CartViewAll = () => {
+
     return (
         <section className='cart_view_all'>
+        <div>
+
+        </div>
 
         </section>
     );
