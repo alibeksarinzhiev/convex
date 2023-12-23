d9adedae075878dbc495e1101df351cd8a0b4b96
adilet <adik.it22run@gmail.com>
2023-12-23 19:44:19 +0600

    'jjj'

--- src/Components/Cart__goods.js
+++ src/Components/Cart__goods.js
@@ -5,7 +5,7 @@ import img from "../img/5d3eee44c3e1b.jpg";
 const CartGoods = () => {
     return (
         <div className='cart__goods'>
-            <h2>Картофель</h2>
+            <h2 className='name active'>Картофель</h2>
             <img src={img} alt=""/>
             <h3>300 сом</h3>
             <p>1 килограмм</p>
